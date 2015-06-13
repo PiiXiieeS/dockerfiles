10e15318bad0fbdd76eddc8756b1421ad4f77a32
Christian-Maximilian Steier <christian@learnlab.soz-kult.fh-duesseldorf.de>
2015-06-14 00:24:03 +0200

    fix

--- hiawatha/scripts/run.sh
+++ hiawatha/scripts/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sed -i "s*DOMAIN*${VIRTUAL_HOST}*g" /etc/hiawatha/hiawatha.conf
+if [ -z "$VIRTUAL_HOST" ]; then
+        sed -i "s*DOMAIN*0.0.0.0*g" /etc/hiawatha/hiawatha.conf
+else
+        sed -i "s*DOMAIN*$VIRTUAL_HOST*g" /etc/hiawatha/hiawatha.conf
+fi
 exec supervisord -n
