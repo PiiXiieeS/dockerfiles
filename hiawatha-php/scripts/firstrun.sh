fb01d7d344218b1e274b7a25f5986da4772c44db
Christian-Maximilian Steier <christian@learnlab.soz-kult.fh-duesseldorf.de>
2015-08-19 20:51:19 +0200

    Update

--- hiawatha-php/scripts/firstrun.sh
+++ hiawatha-php/scripts/firstrun.sh
@@ -1,15 +1,11 @@
+#!/bin/bash
 
 DOMAIN=${DOMAIN:-domain.org}
 
-pre_start_action() {
   echo "DOMAIN=$DOMAIN"
   if [ -z "$DOMAIN" ]; then
 	sed -i "s*DOMAIN*0.0.0.0*g" /etc/hiawatha/hiawatha.conf
   else
 	sed -i "s*DOMAIN*$DOMAIN*g" /etc/hiawatha/hiawatha.conf
   fi
-}
-
-post_start_action() {
   rm /firstrun
-}
--- hiawatha-php/service/hiawatha.sh
+++ hiawatha-php/service/hiawatha.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-# Stop on error
-set -e
-
-if [[ -e /firstrun ]]; then
-  source /scripts/first_run.sh
-else
-  source /scripts/normal_run.sh
-fi
-
-run_post_start_action() {
-  post_start_action
-}
-
-pre_start_action
-
-run_post_start_action &
 
 service hiawatha start
 
