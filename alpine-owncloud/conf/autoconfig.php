a336efce8989b034f346c80eeb10edc0dbfd2623
root <root@datenbox.fritz.box>
2015-11-22 17:50:02 -0700

    Alpine ownCloud

---
+++ alpine-owncloud/conf/autoconfig.php
@@ -0,0 +1,13 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "DBNAME",
+  "dbuser"        => "DBUSER",
+  "dbpass"        => "DBPASS",
+  "dbhost"        => "DBHOST",
+  "dbtableprefix" => "",
+  "directory"     => "/data",
+  "adminlogin"    => "OCADMIN",
+  "adminpass"     => "OCPASS",
+  "passwordsalt"  => "OCSALT",
+);
