e7eff84f5ff9dd7631de0885a522ce75e54b4e13
Christian Steier <christiansteier@users.noreply.github.com>
2016-04-25 20:58:09 +0200

    Update architecture.sh

--- alpine-base/scripts/architecture.sh
+++ alpine-base/scripts/architecture.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(uname -m) == "armv6l" ]; then echo "armhf" > ./conf/ARCHTAG ; else echo $(uname -m) > ./conf/ARCHTAG ; fi
+if [ $(uname -m) == "armv6l" ]; then echo "armhf" > ./conf/ARCHTAG ; elif [ $(uname -m) == "armv7l" ]; then echo "armhf" > ./conf/ARCHTAG ; else echo $(uname -m) > ./conf/ARCHTAG ; fi
 ./scripts/placeholder.sh
 
 exit 0
