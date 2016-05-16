e2dd8c2a30163f1fe4814acff185cf5d227a4c3e
Christian Steier <christiansteier@users.noreply.github.com>
2016-05-16 09:49:28 +0200

    Create cleanup.sh

---
+++ alpine-www/hiawatha/scripts/cleanup.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+sysdirs="
+  /bin
+  /etc
+  /lib
+  /sbin
+  /usr
+"
+find $sysdirs -xdev -regex '.*apk.*' -exec rm -fr {} +
+find $sysdirs -xdev -type f -regex '.*-$' -exec rm -f {} +
+find $sysdirs -xdev -type l -exec test ! -e {} \; -delete
+find $sysdirs -xdev -type d \
+  -exec chown root:root {} \; \
+  -exec chmod 0755 {} \;
+find $sysdirs -xdev -type f -a -perm +4000 -delete
+find $sysdirs -xdev \( \
+  -name hexdump -o \
+  -name chgrp -o \
+  -name ln -o \
+  -name od -o \
+  -name strings -o \
+  -name su \
+  \) -delete
+
+sed -i -r '/^(www-data|hiawatha|root)/!d' /etc/group
+sed -i -r '/^(hiawatha|root)/!d' /etc/passwd
+sed -i -r 's#^(.*):[^:]*$#\1:/sbin/nologin#' /etc/passwd
+
+rm -rf /var/cache/apk/* /usr/share/doc /usr/share/man/ /usr/share/info/* /var/cache/man/* /var/tmp /tmp /etc/fstab
+rm -fr /etc/init.d /lib/rc /etc/conf.d /etc/inittab /etc/runlevels /etc/rc.conf 
+rm -fr /etc/sysctl* /etc/modprobe.d /etc/modules /etc/mdev.conf /etc/acpi
+
+exit 0
