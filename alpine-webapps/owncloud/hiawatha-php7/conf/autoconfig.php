f0ab13f387c12db4b18f62147d44b90ec1d1c3cf
root <root@cloud.steier.berlin>
2016-06-07 06:25:27 +0000

    owncloud PHP7

---
+++ alpine-webapps/owncloud/hiawatha-php7/conf/autoconfig.php
@@ -0,0 +1,13 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "DBNAME",
+  "dbuser"        => "DBUSER",
+  "dbpass"        => "DBPASS",
+  "dbhost"        => "DBHOST",
+  "dbtableprefix" => "",
+  "directory"     => "/var/owncloud/data",
+  "adminlogin"    => "OCADMIN",
+  "adminpass"     => "OCPASS",
+  "passwordsalt"  => "OCSALT",
+);
