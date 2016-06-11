0592b75444fdd3515b9b11ad49ee9c6ca157923e
Christian-Maximilian Steier <christian@learnlab.soz-kult.fh-duesseldorf.de>
2016-06-11 23:59:07 +0200

    Add NGINX

--- hiawatha/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$VIRTUAL_HOST" ]; then
-        sed -i "s*DOMAIN*0.0.0.0*g" /etc/hiawatha/hiawatha.conf
-else
-        sed -i "s*DOMAIN*$VIRTUAL_HOST*g" /etc/hiawatha/hiawatha.conf
-fi
-exec supervisord -n
