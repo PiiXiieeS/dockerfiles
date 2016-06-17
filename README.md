b20272f1c733ade210383a78e7b22b4803152a04
Christian-Maximilian Steier <christian.steier@hs-duesseldorf.de>
2016-06-17 10:56:38 +0200

    Small bug fixes

---
+++ README.md
@@ -0,0 +1,6 @@
+# docker-debian (x86_64 and armhf)
+Debian based images with S6 for Docker on regular Server (x86_64) and RaspberryPi (armhf)
+
+```
+git clone -b debian https://github.com/christiansteier/dockerfiles.git
+```
--- debian-base/scripts/debootstrap.sh
+++ debian-base/scripts/debootstrap.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-TAGPREFIX=maxder/debian-mini:
+if [ -d /usr/share/docker-engine/contrib]; then
+  MKIMAGE=/usr/share/docker-engine/contrib/mkimage.sh
+else
+  curl -o /tmp/maxder_debian-mini_mkimage.sh -L https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage.sh
+  chmod +x /tmp/maxder_debian-mini_mkimage.sh
+  MKIMAGE=/tmp/maxder_debian-mini_mkimage.sh
+fi
+
+TAGPREFIX=maxder/debian-mini
 SUITES="jessie"
 
 for suite in ${SUITES}; do 
-  /usr/share/docker-engine/contrib/mkimage.sh -t ${TAGPREFIX}${suite} debootstrap --variant=minbase ${suite} http://httpredir.debian.org/debian
+  $MKIMAGE -t ${TAGPREFIX}:ARCHTAG debootstrap --variant=minbase ${suite} http://httpredir.debian.org/debian
 done
