322fdc7cc5f7271d468337f50e08a3fc48dab519
Christian Steier <christiansteier@users.noreply.github.com>
2016-06-17 11:01:14 +0200

    Update debootstrap.sh

--- debian-base/scripts/debootstrap.sh
+++ debian-base/scripts/debootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -d /usr/share/docker-engine/contrib]; then
-  MKIMAGE=/usr/share/docker-engine/contrib/mkimage.sh
+if [ -d "/usr/share/docker-engine/contrib" ]; then
+  MKIMAGE="/usr/share/docker-engine/contrib/mkimage.sh"
 else
   curl -o /tmp/maxder_debian-mini_mkimage.sh -L https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage.sh
   chmod +x /tmp/maxder_debian-mini_mkimage.sh
-  MKIMAGE=/tmp/maxder_debian-mini_mkimage.sh
+  MKIMAGE="/tmp/maxder_debian-mini_mkimage.sh"
 fi
 
 TAGPREFIX=maxder/debian-mini
